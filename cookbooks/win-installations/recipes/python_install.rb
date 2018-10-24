255c0503d74d02976bfa3d0aea3f2d09deac7195
Md Inzamamul Haque <inzamamul.haque@relevancelab.com>
2018-10-24 18:45:41 +0530

    created python_install.rb

---
+++ cookbooks/win-installations/recipes/python_install.rb
@@ -0,0 +1,9 @@
+remote_file 'C:\temp\installers\python-3.7.1-amd64.exe' do
+   source 'https://www.python.org/ftp/python/3.7.1/python-3.7.1-amd64.exe'
+end
+
+windows_package 'python' do
+  source 'C:\temp\installers\python-3.7.1-amd64.exe'
+  installer_type :custom
+  options '/Q'
+end
