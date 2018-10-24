e33ddba0bf25d16c2188d638437247704627b4fe
Md Inzamamul Haque <inzamamul.haque@relevancelab.com>
2018-10-24 19:01:01 +0530

    updated python_install.rb

--- cookbooks/win-installations/recipes/default.rb
+++ cookbooks/win-installations/recipes/default.rb
@@ -1,5 +1,10 @@
-#
-# Cookbook:: win-installations
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
+remote_file 'C:\temp\installers\python-3.7.1-amd64.exe' do
+   source 'https://www.python.org/ftp/python/3.7.1/python-3.7.1-amd64.exe'
+   mode '0755'
+end
+
+windows_package 'python' do
+  source 'C:\temp\installers\python-3.7.1-amd64.exe'
+  installer_type :custom
+  options '/Q'
+end
