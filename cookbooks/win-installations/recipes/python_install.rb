7872fa1fe7d42b9bb7afd0da0527d0282ddaa943
Md Inzamamul Haque <inzamamul.haque@relevancelab.com>
2018-10-24 18:51:26 +0530

    updated python_install.rb

--- cookbooks/win-installations/recipes/python_install.rb
+++ cookbooks/win-installations/recipes/python_install.rb
@@ -1,5 +1,6 @@
 remote_file 'C:\temp\installers\python-3.7.1-amd64.exe' do
    source 'https://www.python.org/ftp/python/3.7.1/python-3.7.1-amd64.exe'
+   mode '0755'
 end
 
 windows_package 'python' do
