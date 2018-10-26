3bd6c8ef39caee060213e4e71d1f78ca8f190335
Inzamamul Haque <mihaque313@gmail.com>
2018-10-26 12:30:06 +0530

    added recipe in win_installations for npp installation

---
+++ cookbooks/win-installations/recipes/npp.rb
@@ -0,0 +1,17 @@
+directory 'C:\\Program Files\\notepad++' do
+  #owner 'root'
+  #group 'root'
+  mode '0755'
+  action :create
+end
+
+remote_file 'C:\\Program Files\\notepad++\\npp.7.5.9.Installer.x64.exe' do
+  source 'https://notepad-plus-plus.org/repository/7.x/7.5.9/npp.7.5.9.Installer.x64.exe'
+  mode '0755'
+end
+
+windows_package 'notepad++' do
+  source 'C:\\Program Files\\notepad++\\npp.7.5.9.Installer.x64.exe'
+  action :install
+  options '/Q'
+end
