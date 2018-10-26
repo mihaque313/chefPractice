7f11619ba747cb592daaeaf1608937cde9e66b6b
Inzamamul Haque <mihaque313@gmail.com>
2018-10-26 18:29:07 +0530

    added recipe in win-installations for internet explorer installation

---
+++ cookbooks/win-installations/recipes/ie.rb
@@ -0,0 +1,21 @@
+ieversion1 = powershell_out('(Get-ItemProperty "HKLM:\\SOFTWARE\\Microsoft\\Internet Explorer" -name svcupdateversion).svcupdateversion').stdout.split('.').first(1)
+puts "#{ieversion1}"
+if ieversion1 -lt 11
+  directory 'C:\\Program Files\\Internet-Explorer' do
+    #owner 'root'
+    #group 'root'
+    mode '0755'
+    action :create
+  end
+
+  remote_file 'C:\\Program Files\\Internet-Explorer\\IE11-Windows6.1-x64-en-us.exe' do
+    source 'https://download.microsoft.com/download/7/1/7/7179A150-F2D2-4502-9D70-4B59EA148EAA/IE11-Windows6.1-x64-en-us.exe'
+    mode '0755'
+  end
+
+  windows_package 'Internet-Explorer' do
+    source 'C:\\Program Files\\Internet-Explorer\\IE11-Windows6.1-x64-en-us.exe'
+    action :install
+    options '/Q'
+  end
+end
